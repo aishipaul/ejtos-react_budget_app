7ac530cab1975d1a44e1c93914a4458baea8385d
aishipaul <2006203@kiit.ac.in>
2023-07-28 15:37:55 -0400

    Done

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext'; 
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 const Remaining = () => {
-    const { expenses, budget } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
-    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
-    return (
-        <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
-        </div>
-    );
+  const { expenses, budget, Currency } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
+  const remaining = budget - totalExpenses;
+
+  const alertType = remaining < 0 ? "alert-danger" : "alert-success";
+
+  const handleAlert = () => {
+    if (remaining < 0) {
+      alert("You have exceeded your budget!");
+    }
+  };
+
+  return (
+    <div className={`alert ${alertType}`} onClick={handleAlert}>
+      <span>
+        Remaining: {Currency} {remaining}
+      </span>
+    </div>
+  );
 };
+
 export default Remaining;
